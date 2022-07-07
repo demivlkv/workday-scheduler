6698af55ee35e6070be789a26494112a75859cd8
Demi Hayashi <demi.h@me.com>
2022-07-07 14:38:52 -0700

    add bg color function in js according to time

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,13 +18,33 @@ const hoursDisplay = [
     { time: "5:00 PM", event: ""}
 ]
 
+const now = moment().format('H A');
+
 // create hour blocks
 hoursDisplay.forEach(function(timeBlock, index) {
     const hourDisplay = timeBlock.time;
+    const colorBlock = bgColor(hourDisplay);
 
-    $('.container').append('<div class="row time-block" id="' + index + '"><div class="col-2 hour">' + hourDisplay + '</div><textarea class="col-8 description">' + timeBlock.event + '</textarea><div class="col-2 saveBtn"><button type="submit"><span class="oi oi-pencil"></span></button></div></div>');
+    // create div for every hour
+    $('.container').append('<div class="row time-block" id="' + index + 
+    '"><div class="col-2 hour">' + hourDisplay + 
+    '</div><textarea class="col-8 description ' + colorBlock + '">' + timeBlock.event + 
+    '</textarea><div class="col-2 saveBtn"><button type="submit"><span class="oi oi-pencil"></span></button></div></div>');
 });
 
+// change color background according to time
+function bgColor(time) {
+    let currentTime = moment(now, "H A");
+    let eventPlan = moment(time, "H A")
+    console.log(currentTime);
+    if (currentTime.isBefore(eventPlan) === true) {
+        return 'future';
+    } else if (currentTime.isAfter(eventPlan) === true) {
+        return 'past';
+    } else {
+        return 'present';
+    }
+};
 // store & load tasks
 
 // create save button
